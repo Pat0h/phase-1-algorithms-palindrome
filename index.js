3f7b1dce8cc56d8dfa4a44812028a5ec4d909b2e
Patrick Ngatia <patrickngatia14@gmail.com>
2023-10-16 16:38:33 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLowerCase();
+
+  // Initialize two pointers, one at the beginning and one at the end of the word
+  let start = 0;
+  let end = word.length - 1;
+
+  // Continue comparing characters while the start pointer is less than the end pointer
+  while (start < end) {
+    // If the characters at the start and end positions are not the same, it's not a palindrome
+    if (word[start] !== word[end]) {
+      return false;
+    }
+
+    // Move the pointers towards each other
+    start++;
+    end--;
+  }
+
+  // If the loop completes without finding any non-matching characters, it's a palindrome
+  return true;
 }
 
 /* 
   Add your pseudocode here
 */
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
 
 /*
   Add written explanation of your solution here
